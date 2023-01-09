cb6236ca3ce564bef32f65f6dd5395e675056794
Daniel John Idorot <ido20002@byui.edu>
2023-01-09 16:51:54 +0800

    Initial commit

--- Journal/Program.cs
+++ Journal/Program.cs
@@ -1,7 +1,37 @@
 ﻿using System;
 
-class Program {
-    static void Main(string[] args) {
-        Console.Write("Hello World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            // Prompt
+            
+
+
+            Console.Write("Please select one of the following choices:\n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+            int choices = int.Parse(Console.ReadLine());
+
+            if (choices == 1) {
+
+            }
+
+            else if (choices == 2) {
+
+            }
+
+            else if (choices == 3) {
+
+            }
+
+            else if (choices == 4) {
+
+            }
+
+            else if (choices == 5) {
+                break;
+            }
+        }
     }
 }
