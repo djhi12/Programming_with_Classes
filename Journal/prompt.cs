46ffa62a80c5f5cf31bf04b6fadc8b7941e75523
Daniel John Idorot <ido20002@byui.edu>
2023-01-09 16:58:48 +0800

    initial commit

--- Journal/prompt.cs
+++ Journal/prompt.cs
@@ -1,16 +1,9 @@
 using System;
-using System.Collections.Generic;
+
 public class prompt {
     public string _promptQuestions;
 
     public void promptDisplay() {
-        var _promptQuestions = new List<string>();
-        promptQuestions.Add("Who was the most interesting person I interacted with today?");
-        promptQuestions.Add("What was the best part of my day?");
-        promptQuestions.Add("How did I see the hand of the Lord in my life today?");
-        promptQuestions.Add("What was the strongest emotion I felt today?");
-        promptQuestions.Add("If I had one thing I could do over today, what would it be?");
-
-
+        Console.Write($"_promptQuestions");
     }
 }
