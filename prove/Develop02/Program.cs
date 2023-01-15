65aae5119b2774a6a1ce827f745ffc2bace84b8e
Daniel John Idorot <ido20002@byui.edu>
2023-01-15 08:24:38 +0800

    Initial commit

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+         while (true)
+        {
+
+            Console.Write("Please select one of the following choices:\n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit\nWhat would you like to do? ");
+
+            int choices = int.Parse(Console.ReadLine());
+
+            if (choices == 1)
+            {
+                string filename = "listQuestions.txt";
+                string[] lines = System.IO.File.ReadAllLines(filename);
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(",");
+
+                    string firstName = parts[0];
+                    string lastName = parts[1];
+                }
+
+                using (StreamWriter outputFile = new StreamWriter(filename))
+                {
+                    // You can add text to the file with the WriteLine method
+                    outputFile.WriteLine("This will be the first line in the file.");
+
+                    // You can use the $ and include variables just like with Console.WriteLine
+                    string color = "Blue";
+                    outputFile.WriteLine($"My favorite color is {color}");
+                }
+
+            }
+
+            else if (choices == 2)
+            {
+
+            }
+
+            else if (choices == 3)
+            {
+
+            }
+
+            else if (choices == 4)
+            {
+
+            }
+
+            else if (choices == 5)
+            {
+                break;
+            }
+        }
     }
-}
+    }
