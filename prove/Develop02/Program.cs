62bb5aa4705294de01b2ace92efa13baf5284915
Daniel John Idorot <ido20002@byui.edu>
2023-01-16 20:21:57 +0800

    Initial commit

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -9,51 +9,23 @@ class Program
 
     static void Main(string[] args)
     {
+        
         while (true)
-        {   
+        {
 
             Console.Write("Please select one of the following choices:\n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit\nWhat would you like to do? ");
 
             int choices = int.Parse(Console.ReadLine());
-            string filePath = "prompts.csv";
+            PromptGenerator promptGen = new PromptGenerator();
 
-            if (choices == 1)
-            {
+            if (choices == 1) {
 
-                PromptGenerator program = new PromptGenerator();
-
-                program.writeToCSV();
-                program.readFromCSV();
-
-                public void writeToCSV()
-                {
-                    StringBuilder builder = new StringBuilder();
-
-                    string text1 = "Text1";
-                    string text2 = "Text2";
-
-                    builder.AppendLine(string.Format("{0}|{1}", text1, text2));
-
-                    File.WriteAllText(filePath, builder.ToString());
-                }
-
-                public void readFromCSV()
-                {
-                    StreamReader streamReader = new StreamReader(filePath);
-
-                    while (!streamReader.EndOfStream)
-                    {
-                        var line = streamReader.ReadLine();
-                        var values = line.Split();
-
-                        Console.WriteLine("{0}", values[0]);
-                    }
-                }
-
+                promptGen.readFile();
             }
-
+            
             else if (choices == 2)
             {
+
 
             }
 
