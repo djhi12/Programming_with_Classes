53f399d7f82e09a70e37f12c79e2ff266a788d70
Daniel John Idorot <ido20002@byui.edu>
2023-01-16 20:21:55 +0800

    Initial commit

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,28 +1,38 @@
 using System;
+using System.IO;
+using System.Text;
 
 public class PromptGenerator
 {
-    string filePath = "prompts.csv";
-
+    string _filePath = "C:\\Programming_with_Classes\\prove\\Develop02";
+    
     PromptGenerator()
     {
+    }
+
+
+    public void writeFile(){
+        StringBuilder builder = new StringBuilder();
+
+        string text1 = "Text1";
+        string text2 = "Text2";
+
+        builder.AppendLine(string.Format("{0} | {1}", text1, text2));
+
+        File.WriteAllText(_filePath, builder.ToString());
 
     }
 
-   
+    public void readFile(){
+        StreamReader streamReader = new StreamReader(_filePath);
 
-    public void readFromCSV()
-    {
-        StreamReader streamReader = new StreamReader(filePath);
-
-        while (!streamReader.EndOfStream)
-        {
+        while (!streamReader.EndOfStream) {
             var line = streamReader.ReadLine();
             var values = line.Split();
 
             Console.WriteLine("{0}", values[0]);
         }
     }
-
+  
 
 }
