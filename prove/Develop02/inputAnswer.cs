3832de92ec8b94f893fadd8402a2f1c6972152b9
Daniel John Idorot <ido20002@byui.edu>
2023-01-17 13:02:37 +0800

    Initial commit

---
+++ prove/Develop02/inputAnswer.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Text;
+
+public class inputAnswer
+{
+    PromptGenerator promptGen = new PromptGenerator();
+
+    public string _promptQuestions;
+    
+   
+
+    public void inputAns(){
+        // var _promptQuestions = new 
+
+    }
+
+}
