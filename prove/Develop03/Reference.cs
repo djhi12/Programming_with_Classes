47706ab0448a8812064c32f519a5cb9d73d03191
Daniel John Idorot <ido20002@byui.edu>
2023-01-19 13:32:00 +0800

    Initial commit

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+public class Scripture {
+    // private  List<string> scriptureVerse = new List<string>();
+    private _scriptureVerse;
+
+
+    public string GetVerse() {
+        return _scriptureVerse;
+    }
+}
