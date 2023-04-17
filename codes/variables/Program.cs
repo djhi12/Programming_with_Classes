e35dd141a8c3828a198de287e5112a51f8b5a891
Daniel John Idorot <ido20002@byui.edu>
2023-04-17 13:51:20 +0800

    Initial commit

---
+++ codes/variables/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    In C#, a variable is a named memory location used to store a value of a specific type.
+
+    To declare a variable, you need to specify the variable's name and its data type, like this:
+*/
+
+int age; // declares an integer variable named "age"
+
+
+// You can also initialize the variable when you declare it, like this:
+int age = 30; // declares and initializes the variable "age" with a value of 30
+
+
+/*
+    C# supports many different data types for variables, including:
+
+    bool: a Boolean value (true or false)
+    byte: an 8-bit unsigned integer
+    short: a 16-bit signed integer
+    int: a 32-bit signed integer
+    long: a 64-bit signed integer
+    float: a single-precision floating-point number
+    double: a double-precision floating-point number
+    decimal: a decimal number with higher precision than float or double
+    char: a Unicode character
+    string: a sequence of Unicode characters
+*/
+
+// Here's an example that declares and initializes variables of various types:
+bool isTrue = true;
+byte myByte = 255;
+short myShort = -32768;
+int myInt = 123456789;
+long myLong = 123456789012345;
+float myFloat = 3.14f;
+double myDouble = 3.14159265358979;
+decimal myDecimal = 3.1415926535897932384626433832m;
+char myChar = 'A';
+string myString = "Hello, world!";
+
