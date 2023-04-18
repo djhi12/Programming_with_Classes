917ee0588873ad8d92c61c6a18479124e86255d4
Daniel John Idorot <ido20002@byui.edu>
2023-04-18 21:01:57 +0800

    Initial commit

---
+++ codes/functions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+            In C#, a function is a block of code that performs a specific task and can be called from other parts of a program. Functions in C# are also known as methods.
+
+            Functions are defined using the void keyword if they do not return a value, or a specific return type if they do. 
+        */
+        int a = 2;
+        int b = 3;
+
+        int result = AddNumbers(a, b);
+        Console.WriteLine("The sum of {0} and {1} is {2}", a, b, result);
+    }
+
+    static int AddNumbers(int x, int y)
+    {
+        int sum = x + y;
+        return sum;
+    }
+    // In this program, we define a function called AddNumbers that takes two integers as input and returns their sum. We then call this function from the Main method and pass in the values of a and b as arguments. The result of the function call is stored in a variable called result, which is then printed to the console using Console.WriteLine.
+}
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+        if (grade >= 90)
+        {
+            letter = "A";
+            Console.WriteLine("Congratulations, you got an A!");
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+            Console.WriteLine("You got a B.");
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+            Console.WriteLine("You got a C.");
+        }
+        else
+        {
+            letter = "F";
+            Console.WriteLine("You got an F. Better luck next time!");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class. Keep working hard!");
+        }
+
+        Console.WriteLine("Your letter grade is: " + letter);
     }
 }
