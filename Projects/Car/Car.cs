1794fe095def8340520dbec2e9f8295c296bf6c8
Daniel John Idorot <ido20002@byui.edu>
2023-04-24 15:57:08 +0800

    Initial commit

---
+++ Projects/Car/Car.cs
@@ -0,0 +1,15 @@
+class Car {
+    // Data members (fields)
+    public string make;
+    public string model;
+    public int year;
+
+    // Member functions (methods)
+    public void Accelerate() {
+        Console.WriteLine("Faster");
+    }
+
+    public void Brake() {
+        Console.WriteLine("Press Break");
+    }
+}
---
+++ Projects/Car/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyNamespace
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.make = "Honda";
+            myCar.model = "Civic";
+            myCar.year = 2022;
+            myCar.Accelerate();
+
+            Console.WriteLine(myCar.make);
+
+        }
+    }
+}
