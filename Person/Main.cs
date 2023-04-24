463c18d2a99e29536d5655f90ab7f96d24c4f2c6
Daniel John Idorot <ido20002@byui.edu>
2023-04-24 15:50:02 +0800

    Initial commit

---
+++ Person/Main.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Main
+{
+
+    class Main
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person._givenName = "Joseph";
+            person._familyName = "Smith";
+            person.ShowWesternName();
+            person.ShowEasternName();
+        }
+    }
+}
