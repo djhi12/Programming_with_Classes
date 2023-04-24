5028742cb868c2df9e5d9c91e71339ec08c6526d
Daniel John Idorot <ido20002@byui.edu>
2023-04-24 14:23:06 +0800

    Initial commit

---
+++ codes/CSharpNamespace/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program {
+    /*
+        In C#, the statement "using System" is used to import the namespace called "System" into your code. The System namespace provides access to a number of fundamental classes and base types that are commonly used in C# programming.
+
+        For example, the System namespace contains classes like Console, which provides basic input/output functionality, and DateTime, which represents a date and time value. By importing the System namespace using the "using" keyword, you can use these classes in your code without having to specify the full namespace each time you reference them.
+    */
+
+    /*
+        In addition to the "System" namespace, C# provides a number of other namespaces that contain classes and types for various purposes. Here are a few examples:
+
+        "System.Collections": This namespace provides classes for working with collections of objects, such as arrays, lists, and dictionaries.
+
+        "System.IO": This namespace provides classes for working with input and output operations, such as reading and writing files and streams.
+
+        "System.Net": This namespace provides classes for working with network protocols and communications, such as TCP/IP, HTTP, and FTP.
+
+        "System.Threading": This namespace provides classes for working with threads and synchronization, such as creating and managing threads and locking resources.
+
+        "System.Xml": This namespace provides classes for working with XML documents and data, such as parsing and generating XML, and transforming XML with XSLT.
+
+        These are just a few examples of the many namespaces available in C#. Depending on the type of application you are building, you may need to use one or more of these namespaces, or even create your own custom namespaces for your code.
+    */
+    static void Main() {
+        Console.WriteLine("Hello, world!");
+    }
+}
---
+++ codes/objects/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+    In C#, "object" is a class that serves as the base class for all other classes in the .NET Framework. It is defined in the System namespace and provides a common set of methods and properties that are inherited by all other classes.
+
+    The "object" class defines several important methods that are used throughout C# programming. These include the ToString method, which returns a string representation of the object, and the Equals method, which is used to compare two objects for equality.
+
+    Because all classes in C# ultimately derive from the "object" class, you can use any object as an argument to a method or as the return value of a method. This makes it possible to create methods that can accept any type of object as input, which can be useful in certain situations.
+
+    In addition to providing a common set of methods and properties, the "object" class also provides a way to box value types, such as integers or floats, so that they can be treated as objects. This is important when working with interfaces or other scenarios where an object is expected, but a value type needs to be passed in.
+
+    Overall, the "object" class is a fundamental part of C# programming and is used extensively throughout the .NET Framework.
+*/
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        object obj1 = new object(); // create a new object
+        object obj2 = new object();
+
+        Console.WriteLine(obj1.ToString()); // call the ToString method on obj1
+        Console.WriteLine(obj1.Equals(obj2)); // call the Equals method on obj1 to compare with obj2
+
+        int num = 5; // create a value type
+        object boxedNum = num; // box the value type in an object
+
+        Console.WriteLine(boxedNum.GetType()); // call the GetType method on boxedNum to get its type
+    }
+}
