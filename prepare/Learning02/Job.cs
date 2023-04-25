d68b4edddbb4f2c2925ac7c38ec76cc7df8d8f97
Daniel John Idorot <ido20002@byui.edu>
2023-04-25 19:33:11 +0800

    Initial commit

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,4 +1,11 @@
 public class Job
 {
-   
+    public string _jobTitle = "";
+    public string _company = "";
+    public string _year = "";
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_year}");
+    }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-      
+        // Job 1
+        Job job1 = new Job();
+        job1._jobTitle = "\nSoftware Engineer";
+        job1._company = "Microsoft";
+        job1._year = "2019-2022";
+        job1.Display();
+
+
+        // Job 2
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._year = "2022-2023\n";
+        job2.Display();
     }
 }
