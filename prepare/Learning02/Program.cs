80b14b306b98ef7715a16a9d834768c2c7cbdd95
Daniel John Idorot <ido20002@byui.edu>
2023-04-25 19:15:51 +0800

    Initial commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,14 +1,9 @@
 using System;
 
-public class Job 
-{
-    _jobTitle
-}
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+      
     }
 }
