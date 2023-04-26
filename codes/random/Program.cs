8f34094f2fdc49d581dab4e6fa6921ec1b171646
Daniel John Idorot <ido20002@byui.edu>
2023-04-26 23:13:15 +0800

    Initial commit

---
+++ codes/random/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+    To show a random prompt in C#, you can create an array or a list of prompts and use the Random class to generate a random index that corresponds to one of the prompts.
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] prompts = { "What's your favorite color?", "What's your favorite food?", "What's your favorite animal?" };
+
+        Random random = new Random();
+        int index = random.Next(prompts.Length);
+
+        Console.WriteLine(prompts[index]);
+    }
+}
