447ddc0a1734c81a4e0b4d208c6deb4a875c1dc5
Daniel John Idorot <ido20002@byui.edu>
2023-05-02 22:34:17 +0800

    Initial commit

--- week3/Program.cs
+++ week3/Program.cs
@@ -1,60 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
+
+public class Account
+{
+    public List<int> _transactions = new List<int>(); // if we change this to a list...
+
+    public void Deposit(int amount)
+    {
+        _transactions.Add(amount);
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Console.Write("Hello");
-        // Account account = new Account();
-        // account.balance = 50;
-        // account.Deposit(100);
-
-        // account.Deposit(100);
-
-
-        // account.balance = 50; // if we make the change above, this doesn't work anymore!
-        // account.Deposit(100); // however, this approach would continue to work
-
-
-        //     private List<int> transactions = new List<int>();
-
-        // public void Deposit(int amount)
-        //     {
-        //      transactions.Add(amount);
-        //     }
-
-
-        // private string _title;
-        // private string _firstName;
-        // private string _lastName;
-
-        // account.GetInformalSignature();
-
-
-
-        // Getter and Setters
-        // Person p = new Person();
-        // p.SetFirstName("Peter");
-
-        // Console.WriteLine(p.GetFirstName());
-        // Create a list of strings  
-        var countries = new List<string>();
-        countries.Add("India");
-        countries.Add("Australia");
-        countries.Add("Japan");
-        countries.Add("Canada");
-        countries.Add("Mexico");
-
-        // Iterate list element using foreach loop  
-        foreach (var country in countries)
-        {
-            Console.WriteLine(country[0]);
-        }
-        Console.ReadLine();
-        Console.Clear();
-
-
-
+        Account savings = new Account();
+        savings._balance = 50; // if we make the change above, this doesn't work anymore!
+        savings.Deposit(100); // however, this approach would continue to work
     }
 }
