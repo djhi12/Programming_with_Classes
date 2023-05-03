54b6b45e5dc64796cba33d2824d26e8c9d037d9c
Daniel John Idorot <ido20002@byui.edu>
2023-05-03 23:20:07 +0800

    Initial commit

---
+++ codes/private/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+    In C#, the private keyword is an access modifier used to limit the visibility of a class member (field, property, method or nested type) to the class or struct where it is declared.
+
+    Members declared as private are accessible only within the same class or struct and cannot be accessed from outside the class. This helps to hide the implementation details of a class or struct and prevent unintended modification or misuse of the member by other code in the application.
+*/
+
+using System;
+
+public class BankAccount
+{
+    private string accountNumber;
+    private decimal balance;
+
+    public BankAccount(string accountNumber, decimal initialBalance)
+    {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds.");
+        }
+    }
+
+    public void DisplayAccountInfo()
+    {
+        Console.WriteLine("Account number: {0}", accountNumber);
+        Console.WriteLine("Balance: {0}", balance);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        BankAccount myAccount = new BankAccount("12345", 1000.00m);
+
+        myAccount.Deposit(500.00m);
+        myAccount.Withdraw(2000.00m);
+        myAccount.DisplayAccountInfo();
+    }
+}
+
+/*
+    In the above program, the BankAccount class has two private fields, accountNumber and balance, which can only be accessed within the class. The Deposit and Withdraw methods modify the balance field, while the DisplayAccountInfo method displays the values of both fields.
+
+    In the Main method, a BankAccount object is created and its Deposit, Withdraw, and DisplayAccountInfo methods are called to demonstrate how the class works. Since the accountNumber and balance fields are private, they cannot be accessed or modified directly from outside the class.
+*/
---
+++ codes/this/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+    
+In C#, the "this" keyword is a reference to the current instance of the class. It is commonly used to differentiate between class-level variables and local variables with the same name.
+
+*/
+
+using System;
+
+class MyClass {
+  private int myVar;
+
+  public void SetMyVar(int value) {
+    this.myVar = value; // use "this" to refer to the class-level variable
+  }
+
+  public void PrintMyVar() {
+    Console.WriteLine("MyVar = " + this.myVar); // use "this" to refer to the class-level variable
+  }
+}
+
+class Program {
+  static void Main() {
+    MyClass obj = new MyClass();
+    obj.SetMyVar(42);
+    obj.PrintMyVar();
+  }
+}
+
+/*
+    In this program, we define a class called MyClass that has a private class-level variable myVar. The class also has two methods: SetMyVar to set the value of myVar, and PrintMyVar to print the value of myVar.
+
+    In the SetMyVar method, we use the "this" keyword to refer to the class-level variable myVar and set its value to the input parameter value. In the PrintMyVar method, we also use the "this" keyword to refer to the class-level variable myVar and print its value to the console.
+
+    In the Main method, we create an instance of MyClass and call SetMyVar to set the value of myVar to 42. Then, we call PrintMyVar to print the value of myVar to the console. The output of this program should be:
+*/
