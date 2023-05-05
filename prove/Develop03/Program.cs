edf0e865ec763c0b6d71245a342c4f28e25c38ac
Daniel John Idorot <ido20002@byui.edu>
2023-05-05 20:36:12 +0800

    Initial commit

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,15 +1,92 @@
 using System;
+using System.Linq;
+
+// class Reference
+// {
+//     public string Book { get; set; }
+//     public int Chapter { get; set; }
+//     public int StartVerse { get; set; }
+//     public int EndVerse { get; set; }
+
+//     public Reference(string book, int chapter, int startVerse, int endVerse = -1)
+//     {
+//         Book = book;
+//         Chapter = chapter;
+//         StartVerse = startVerse;
+//         EndVerse = endVerse;
+//     }
+
+//     public override string ToString()
+//     {
+//         if (EndVerse > 0)
+//         {
+//             return $"{Book} {Chapter}:{StartVerse}-{EndVerse}";
+//         }
+//         else
+//         {
+//             return $"{Book} {Chapter}:{StartVerse}";
+//         }
+//     }
+// }
+
+// class Scripture
+// {
+//     public Reference Reference { get; set; }
+//     public string Text { get; set; }
+
+//     public Scripture(Reference reference, string text)
+//     {
+//         Reference = reference;
+//         Text = text;
+//     }
+
+//     public string[] GetWords()
+//     {
+//         return Text.Split(' ');
+//     }
+// }
 
 class Program
 {
     static void Main(string[] args)
     {
-        // while(true) {
-            
-        // }
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference, "For aGod so bloved the cworld, that he dgave his eonly begotten fSon, that whosoever gbelieveth in him should not perish, but have heverlasting ilife.");
+        string[] words = scripture.GetWords();
+        int[] hiddenIndices = new int[words.Length];
 
-        Scripture scripture = new Scripture();
+        Console.Clear();
+        Console.WriteLine(scripture.Reference);
+        Console.WriteLine(scripture.Text);
+        Console.WriteLine("Press enter to hide words or type 'quit' to exit.");
+        string input = Console.ReadLine();
 
-        Console.Write($"{scripture.scriptureDisplay()}");
+        while (input.ToLower() != "quit")
+        {
+            // Find words to hide
+            int numWordsToHide = words.Length / 3; // Hide 1/3 of words
+            int[] wordsToHide = Enumerable.Range(0, words.Length)
+                                          .Where(i => hiddenIndices[i] == 0) // Only consider words that are not already hidden
+                                          .OrderBy(i => Guid.NewGuid()) // Randomly sort the list
+                                          .Take(numWordsToHide)
+                                          .ToArray();
+            // Hide words
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (wordsToHide.Contains(i))
+                {
+                    Console.Write(new string('_', words[i].Length) + " ");
+                    hiddenIndices[i] = 1;
+                }
+                else
+                {
+                    Console.Write(words[i] + " ");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press enter to hide more words or type 'quit' to exit.");
+            input = Console.ReadLine();
+        }
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,32 +1,16 @@
-using System;
-using System.Collections.Generic;
-
-public class Scripture
+class Scripture
 {
-    private string _scriptureVerse;
-    private string _referenceVerse;
+    public Reference Reference { get; set; }
+    public string Text { get; set; }
 
-    public string scriptureDisplay()
+    public Scripture(Reference reference, string text)
     {
-        Random random = new Random();
-        List<string> _scriptureVerse = new List<string>() {
-            "For", "behold", ",", "this", "is", "my", "work", "and", "my", "glory", "-", "to", "bring", "to", "pass", "the", "immortality", "and", "eternal", "life", "of", "man."
-        };
+        Reference = reference;
+        Text = text;
+    }
 
-        foreach (var _scriptureWord in _scriptureVerse)
-        {
-            if (_scriptureWord == "")
-            {
-
-            }
-        }
-
-
-
-        _referenceVerse = "Moses 1:39";
-
-        string scriptureRead = $"{_referenceVerse} {_scriptureVerse}";
-
-        return scriptureRead;
+    public string[] GetWords()
+    {
+        return Text.Split(' ');
     }
 }
